227958b97ee5c4e4791debef1504c1028c2b2322
Giovani Miguel <giovanibrmiguel@gmail.com>
2022-07-20 23:11:15 +0000

    Created seeds

--- project-files/dbt/models/staging/stg_country_product_trade.sql
+++ project-files/dbt/models/staging/stg_country_product_trade.sql
@@ -1,9 +1,9 @@
 SELECT
     FARM_FINGERPRINT(CONCAT(location_code, partner_code, CAST(product_id AS STRING), CAST(year AS STRING), CAST(import_value AS STRING), CAST(export_value AS STRING))) AS transaction_id,
-    location_id,
-    partner_id,
-    location_code,
-    partner_code,
+    location_id AS country_id,
+    partner_id AS partner_country_id,
+    location_code AS country_code,
+    partner_code AS partner_country_code,
     product_id,
     export_value,
     import_value,
